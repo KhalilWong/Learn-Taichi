89217aa9aa47c5983d9faf3391d4e91c8d3bf82b
Khalil Wong <1039906090@qq.com>
2021-07-07 13:39:50 +0800

    Learn API

--- test2.py
+++ test2.py
@@ -2,14 +2,22 @@ import taichi as ti
 #
 ti.init(arch = ti.gpu)
 #
+blk1 = ti.root
+blk2 = blk1.dense(ti.i,  3)
+blk3 = blk2.dense(ti.jk, (5, 2))
+blk4 = blk3.dense(ti.k,  2)
+blk1.shape()  # ()
+blk2.shape()  # (3, )
+blk3.shape()  # (3, 5, 2)
+blk4.shape()  # (3, 5, 4)
+#
 @ti.kernel
 def my_kernel():
-    print(x[0])
     for i in x:
-        print(i)
         x[i] = i * 2
 
 x = ti.field(ti.f32, 4)
 my_kernel()
 x_np = x.to_numpy()
 print(x_np)  # np.array([0, 2, 4, 6])
+################################################################################
