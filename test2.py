4b0f0cfb339bc7935269a6df796d0522a1fc80fe
Khalil Wong <1039906090@qq.com>
2021-07-07 09:55:01 +0800

    test1,2

---
+++ test2.py
@@ -0,0 +1,15 @@
+import taichi as ti
+#
+ti.init(arch=ti.gpu)
+#
+@ti.kernel
+def my_kernel():
+    print(x[0])
+    for i in x:
+        print(i)
+        x[i] = i * 2
+
+x = ti.field(ti.f32, 4)
+my_kernel()
+x_np = x.to_numpy()
+print(x_np)  # np.array([0, 2, 4, 6])
